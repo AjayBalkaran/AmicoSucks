a608334bf152b3ad4e78169d2ee2ef3dc448b775
Ajay Balkaran <49107213+AjayBalkaran@users.noreply.github.com>
2023-09-18 13:13:42 -0400

    Update JavaScript and HTML
    In this commit, various enhancements were made to both the JavaScript and HTML code:
    
    1. Added a new `calculateResults()` function in JavaScript to handle calculations when the "Calculate" button is clicked.
    
    2. Changed the `onclick` function of the "Calculate" button in the HTML to call the `calculateResults()` function.
    
    3. Updated the HTML structure and labels to match the functionality.
    
    These changes improve the functionality of the web page, allowing users to calculate results more efficiently.

--- main.js
+++ main.js
@@ -1,79 +1,117 @@
 // Variables
 const inputElements = {
-    list: document.getElementById('listInput'),
-    net: document.getElementById('netInput'),
-    removal: document.getElementById('removalInput'),
-    valveDiscount: document.getElementById('valveDiscount'),
-    lineItemCondition: document.getElementById('lineItemCondition'),
-    overallDiscount: document.getElementById('overallDiscount'),
-  };
-  
-  const resultElement = document.getElementById('result');
-  
-  // Function to Display Results
-  function displayResults(text) {
-    resultElement.textContent = text;
-    console.log('Results are being displayed');
-  }
-  
-  // Function to Calculate Discount
-  function calculateDiscount(netAmount, listAmount) {
-    const discount = 100 * (netAmount / listAmount);
-    return discount;
-  }
-  
-  // Function to Calculate Discount from static discount after Removing a Set Price
-  function removePrice() {
-    const list = inputElements.list.value;
-    const net = inputElements.net.value;
-    const removal = inputElements.removal.value;
-  
-    const newNet = net - removal;
-  
-    return calculateDiscount(newNet, list);
-  }
-  
-  // Function to Calculate Discount from multiple discounts after Removing a Set Price
-  function removePriceMultipal() {
-    const list = inputElements.list.value;
-    const net = inputElements.net.value;
-    const removal = inputElements.removal.value;
-    const overallDiscount = inputElements.overallDiscount.value / 100;
-    const valveDiscount = inputElements.valveDiscount.value / 100;
-    const lineItemCondition = inputElements.lineItemCondition.value;
-  
-    const newNet = net - (lineItemCondition / valveDiscount) * (1 - valveDiscount);
-    const newList = newNet / (1 - overallDiscount);
-    const desiredPrice = newNet - removal;
-  
-    return parseFloat(100 - calculateDiscount(desiredPrice, newList)).toFixed(3);
-  }
-  
-  // Testing Function
-  function testScenario(scenario) {
-    if (scenario === 1) {
-      console.log(inputElements.list.value); // Testing if the document value is being taken.
-    } else if (scenario === 2) {
-      console.log(resultElement.textContent); // Testing if the result text is being targeted.
-    } else if (scenario === 3) {
-      inputElements.list.value = 3746;
-      inputElements.net.value = 1550.25;
-      inputElements.valveDiscount.value = 55;
-      inputElements.lineItemCondition.value = 570.35;
-      inputElements.overallDiscount.value = 60;
-      inputElements.removal.value = 155.03;
-  
-      if (removePriceMultipal() === '65.723') {
-        console.log('Success');
-      } else {
-        console.log('Something went wrong');
-      }
-    } else if (scenario === 4) {
-      // Testing the removePrice() function
-      inputElements.list.value = 100;
-      inputElements.net.value = 55;
-      inputElements.removal.value = 5;
-      resultElement.textContent = `To remove $${inputElements.removal.value}, we would need to change the discount to ${removePrice()}`;
+  list: document.getElementById('listInput'),
+  net: document.getElementById('netInput'),
+  removal: document.getElementById('removalInput'),
+  valveDiscount: document.getElementById('valveDiscount'),
+  lineItemCondition: document.getElementById('lineItemCondition'),
+  overallDiscount: document.getElementById('overallDiscount'),
+};
+
+const resultElement = document.getElementById('result');
+let answer; // This will be used to store the new discount value
+
+// Arrays
+const resultsArr = [
+  () => {
+    if (typeof answer !== 'undefined') {
+      return `This is the result for a single discount: ${answer}`;
+    } else {
+      return "Answer is not defined.";
     }
+  },
+  () => {
+    if (typeof answer !== 'undefined') {
+      return `This is the result for multiple discounts: ${answer}`;
+    } else {
+      return "Answer is not defined.";
+    }
+  },
+  "THIS IS JUST A TEST RESULT"
+];
+
+// Function to Display Results
+function displayResults(text) {
+  resultElement.textContent = text;
+  console.log('Results are being displayed');
+}
+
+// Function to Calculate Discount
+function calculateDiscount(netAmount, listAmount) {
+  const discount = 100 * (netAmount / listAmount);
+  return discount;
+}
+
+// Function to Calculate Discount from static discount after Removing a Set Price
+function removePrice() {
+  const list = parseFloat(inputElements.list.value);
+  const net = parseFloat(inputElements.net.value);
+  const removal = parseFloat(inputElements.removal.value);
+
+  if (isNaN(list) || isNaN(net) || isNaN(removal)) {
+    displayResults("Please enter valid numbers in all required fields.");
+    return;
   }
-  
+
+  const newNet = net - removal;
+
+  answer = calculateDiscount(newNet, list);
+  displayResults(resultsArr[0]());
+}
+
+// Function to Calculate Discount from multiple discounts after Removing a Set Price
+function removePriceMultipal() {
+  const list = parseFloat(inputElements.list.value);
+  const net = parseFloat(inputElements.net.value);
+  const removal = parseFloat(inputElements.removal.value);
+  const overallDiscount = parseFloat(inputElements.overallDiscount.value) / 100;
+  const valveDiscount = parseFloat(inputElements.valveDiscount.value) / 100;
+  const lineItemCondition = parseFloat(inputElements.lineItemCondition.value);
+
+  if (isNaN(list) || isNaN(net) || isNaN(removal) || isNaN(overallDiscount) || isNaN(valveDiscount) || isNaN(lineItemCondition)) {
+    displayResults("Please enter valid numbers in all required fields.");
+    return;
+  }
+
+  const newNet = net - (lineItemCondition / valveDiscount) * (1 - valveDiscount);
+  const newList = newNet / (1 - overallDiscount);
+  const desiredPrice = newNet - removal;
+
+  answer = parseFloat(100 - calculateDiscount(desiredPrice, newList)).toFixed(3);
+  displayResults(resultsArr[1]());
+}
+
+// Function checks if the input field is empty and either returns true or false
+function isFieldEmpty(inputField) {
+  // Get the value of the input field
+  var inputValue = inputField.value;
+
+  // Use regular expression to check for whitespace characters
+  var isBlank = /^\s*$/.test(inputValue);
+
+  if (isBlank) {
+    console.log(`${inputValue} is blank`);
+    return true;
+  } else {
+    console.log(`${inputValue} is not blank`);
+    return false;
+  }
+}
+
+// Results Function
+function calculateResults() {
+  // if statement checks if all necessary fields have been inputted
+  if (!isFieldEmpty(inputElements.net) && !isFieldEmpty(inputElements.list) && !isFieldEmpty(inputElements.removal)) {
+    console.log("All necessary inputs are valid.");
+    let answerText;
+
+    // if statement that checks if the fields required for multiple discounts have been entered
+    if (!isFieldEmpty(inputElements.overallDiscount) && !isFieldEmpty(inputElements.lineItemCondition) && !isFieldEmpty(inputElements.valveDiscount)) {
+      removePriceMultipal();
+    } else { // this is assuming that we are using a flat discount
+      removePrice();
+    }
+  } else {
+    displayResults("Please enter values in all required fields.");
+  }
+}
